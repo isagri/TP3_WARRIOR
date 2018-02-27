1e37638ba27a956df7e77b125bf4c66603b1f1ec
isagri <isabelle.grillet6@orange.fr>
2018-02-27 09:29:29 +0100

    Initialization

---
+++ students/warrior.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . "/../base/localWarrior.php";
+
+
+$GLOBALS['warriorID'] = 'azertyuiop';
+
+// Définissez vos class de combattants
+abstract class Warrior extends LocalWarrior {
+
+};
