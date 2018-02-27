a86456ee57bbaf61cbbef6fe4e8846b65c240cf3
isagri <isabelle.grillet6@orange.fr>
2018-02-27 16:28:45 +0100

    tp terminé

--- students/battleField.php
+++ students/battleField.php
@@ -6,6 +6,23 @@
 
   class BattleField extends BaseBattleField
   {
+      public static function createMyWarrior() {
+        $guerrier = new PokemonWarrior($GLOBALS['warriorID']);
+        $weapon = new Weapon('nunchaku',10); 
+        $weapon->SetImageUrl('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNbb_IpvytfawIbZAfhkh1aOrtr0v9gT5TITTT4K-9k6wYED3w');
+        $guerrier->setweapon($weapon);
+        $guerrier->setImageUrl('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZveRNhXVxhDfdDbCWZp-WPWX5pKz4XSgo5BcVzOte_CQ3de2piQ');
+        $guerrier->saveNew();
+      }
+
+      public static function createOtherWarrior() {
+        $guerrier = new PokemonWarrior('le Méchant');
+        $weapon = new Weapon('arc',10); 
+        $weapon->SetImageUrl('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCdHw3e9Sfzk3quQvQAD-xfy9_i7FVir_Efy7xoWxD54aWcMys');
+        $guerrier->setweapon($weapon);
+        $guerrier->setImageUrl('http://image.jeuxvideo.com/medias-md/150399/1503994693-5390-card.jpg');
+        $guerrier->saveNew();
+      }
 
 
-  }
+    }
--- students/warrior.php
+++ students/warrior.php
@@ -3,9 +3,89 @@
 require_once __DIR__ . "/../base/localWarrior.php";
 
 
-$GLOBALS['warriorID'] = 'azertyuiop';
+$GLOBALS['warriorID'] = 'Zeldisa';
 
 // Définissez vos class de combattants
 abstract class Warrior extends LocalWarrior {
+    public $id;
+    public $name;
+    public $speed;
+    public $life;
+    public $shield;
+    public $imageUrl;
+    public $weapon;
+    public function __construct($id) {
+        $this->id = $id;
+        $this->speed=30;
+        $this->life=100;
+        $this->shield=20;
+    }
+
+    public function SetWeapon (Weapon $weapon) {
+        $this->weapon = $weapon;
+    }
+
+    public function SetImageUrl($url) {
+        $this->imageUrl = $url;
+    }
+
+};
+
+class StartrekWarrior extends Warrior {
+    public $mentalPower;
+
+    public function __construct($id) {
+        parent::__construct($id);
+        $this->mentalPower = 8;
+    }
+
+    public function power() {
+        return $this->mentalPower;
+    }
+
+};
+
+class MarvelWarrior extends Warrior {
+    public $superPower;
+
+    public function __construct($id) {
+        parent::__construct($id);
+        $this->superPower = 100;
+    }
+
+    public function power() {
+        return $this->superPower;
+    }
+
+};
+
+class PokemonWarrior extends Warrior {
+    public $level;
+    
+    public function __construct($id) {
+        parent::__construct($id);
+        $this->level = 1;
+    }
+
+    public function power() {
+        return $this->level;
+    }
+
+};
+
+class Weapon {
+    public $id;
+    public $strength;
+    public $imageUrl;
+
+    public function __construct($id, $strength) {
+        $this->id = $id;
+        $this->strength = $strength;
+    }
+
+    public function SetImageUrl($url) {
+        $this->imageUrl = $url;
+    }
+
 
 };
